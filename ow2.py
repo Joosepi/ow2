7d1f8d7c07694484b4a7fe07c4566e1cfa497b95
Joosepi <martinjoosep.reiljan@ametikool.ee>
2023-03-30 12:02:29 -0700

    ae

--- ow2.py
+++ ow2.py
@@ -1,44 +1,79 @@
-import pyautogui
-import numpy as np
 import cv2
+import mediapipe as mp
+import time
 
-# Set the target color
-target_color = (189, 21, 196)
+mp_drawing = mp.solutions.drawing_utils
+mp_pose = mp.solutions.pose
 
-while True:
-    # Capture the screen and convert to HSV color space
-    screen = np.array(pyautogui.screenshot())
-    hsv = cv2.cvtColor(screen, cv2.COLOR_BGR2HSV)
-    
-    # Threshold the image to get the mask
-    lower = np.array([target_color[0] - 10, 100, 100])
-    upper = np.array([target_color[0] + 10, 255, 255])
-    mask = cv2.inRange(hsv, lower, upper)
-    
-    # Find the contours of the mask
-    contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    
-    # If we found a contour, move the mouse to its center
-    if len(contours) > 0:
-        # Get the center of the first contour
-        M = cv2.moments(contours[0])
-        cx = int(M['m10'] / M['m00'])
-        cy = int(M['m01'] / M['m00'])
-        
-        # Get the screen size
-        screen_size = pyautogui.size()
-        
-        # Calculate the center of the screen
-        screen_center_x = screen_size.width // 2
-        screen_center_y = screen_size.height // 2
-        
-        # Calculate the offset from the center
-        offset_x = cx - screen_center_x
-        offset_y = cy - screen_center_y
-        
-        # Calculate the mouse movement distance
-        move_x = offset_x / screen_center_x * aim_max_move_pixels
-        move_y = offset_y / screen_center_y * aim_max_move_pixels
-        
-        # Move the mouse
-        pyautogui.moveRel(move_x, move_y, duration=aim_duration_millis/1000)
+cap = cv2.VideoCapture(0)
+robot_center = None
+prediction_on = False
+FOV = 100  # Field of View
+
+with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+    while cap.isOpened():
+        ret, frame = cap.read()
+
+        # Flip the image horizontally for a later selfie-view display
+        frame = cv2.flip(frame, 1)
+
+        # Convert the BGR image to RGB.
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        # Set flag to draw the output image with landmarks and connectors
+        if prediction_on:
+            draw = True
+        else:
+            draw = False
+
+        # To improve performance, optionally mark the frame as not writeable to pass by reference.
+        image.flags.writeable = False
+
+        # Process the image and find pose landmarks
+        results = pose.process(image)
+
+        # Set flag to write the output image to the frame if landmarks are detected
+        if results.pose_landmarks and prediction_on:
+            image.flags.writeable = True
+            mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+
+            # Get the center point of the robot
+            x_max = 0
+            x_min = 10000
+            y_max = 0
+            y_min = 10000
+
+            for landmark in results.pose_landmarks.landmark:
+                x, y = int(landmark.x * frame.shape[1]), int(landmark.y * frame.shape[0])
+                if x > x_max:
+                    x_max = x
+                if x < x_min:
+                    x_min = x
+                if y > y_max:
+                    y_max = y
+                if y < y_min:
+                    y_min = y
+
+            robot_center = (int((x_max + x_min) / 2), int((y_max + y_min) / 2))
+
+        # If the robot is within the field of view, draw a circle on it and print a message
+        if robot_center:
+            if frame.shape[1] / 2 - FOV / 2 <= robot_center[0] <= frame.shape[1] / 2 + FOV / 2:
+                if frame.shape[0] / 2 - FOV / 2 <= robot_center[1] <= frame.shape[0] / 2 + FOV / 2:
+                    cv2.circle(frame, robot_center, 10, (0, 255, 0), -1)
+                    print("Robot detected within field of view!")
+
+        # Display the resulting image
+        cv2.imshow('MediaPipe Pose', frame)
+
+        # Check for key press to turn prediction on/off
+        key = cv2.waitKey(1)
+        if key == ord('t'):
+            prediction_on = not prediction_on
+
+        # Break the loop if q is pressed
+        if key == ord('q'):
+            break
+
+cap.release()
+cv2.destroyAllWindows()
